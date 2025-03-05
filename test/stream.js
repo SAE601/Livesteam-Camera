47804ece270acc74ac2bd74da3f8b5880e4aed3b
Guillaume Seimandi <guillaume.seimandi@hotmail.com>
2025-03-05 11:48:56 +0100

    Modification de l'adresse IP

--- test/stream.js
+++ test/stream.js
@@ -1,7 +1,7 @@
 if (Hls.isSupported()) {
     var video = document.getElementById('videoElement');
     var hls = new Hls();
-    hls.loadSource('http://localhost:5000/livestream/stream/video.m3u8');
+    hls.loadSource('http://172.20.10.4:5000/livestream/stream/video.m3u8');
     hls.attachMedia(video);
 } else {
     console.error('HLS n\'est pas supporté par ce navigateur.');
